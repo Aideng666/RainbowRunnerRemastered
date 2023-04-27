0aeb32c18622321c76a79695a706a96d83be3d82
Aiden Gimpel <aiden.gimpel@ontariotechu.net>
2023-04-26 23:33:50 -0400

    Platforms Pick a color when spawned (no collision yet), Layers Created

--- RainbowRunnerRemastered/Assets/Scripts/Platform.cs
+++ RainbowRunnerRemastered/Assets/Scripts/Platform.cs
@@ -4,17 +4,44 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
+    [SerializeField] Material color1Mat;
+    [SerializeField] Material color2Mat;
+    [SerializeField] Material color3Mat;
     [SerializeField] float platformMoveSpeed = 10;
     [SerializeField] bool isStartingPlatform = false;
 
+    MeshRenderer mesh;
+
     private void OnEnable()
     {
-        
+        mesh = GetComponent<MeshRenderer>();
+
+        switch (Random.Range(0, 3))
+        {
+            case 0:
+
+                mesh.material = color1Mat;
+
+                break;
+
+            case 1:
+
+                mesh.material = color2Mat;
+
+                break;
+
+            case 2:
+
+                mesh.material = color3Mat;
+
+                break;
+        }
     }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mesh = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
