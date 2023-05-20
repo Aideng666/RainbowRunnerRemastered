1d9ad5393fdc3734688f3033f9bdb8f9fe99ea9d
Tyler Wong <tywong8@gmail.com>
2023-05-19 22:37:50 -0400

    menu wip

--- RainbowRunnerRemastered/Assets/Scripts/UI/MenuManager.cs
+++ RainbowRunnerRemastered/Assets/Scripts/UI/MenuManager.cs
@@ -7,6 +7,7 @@ using DG.Tweening;
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] List<GameObject> canvasList = new List<GameObject>();
+    //[SerializeField] RectTransform faderImage;
 
     Tween fadeTween;
     public float startFadeTime = 1f;
@@ -15,13 +16,31 @@ public class MenuManager : MonoBehaviour
     void Start()
     {
         StartCoroutine(FadeCanvas());
+        //faderImage.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if (InputManager.Instance.GetInput())
+
     }
+
+
+    //public void FadeTransitionImage()
+    //{
+    //    Sequence sequence = DOTween.Sequence();
+
+    //    sequence.Append(faderImage.DOScale(1f, 1f).SetEase(Ease.InOutExpo))
+    //        .AppendInterval(2f)
+    //        .Append(faderImage.DOScale(0f, 1f).SetEase(Ease.InOutExpo))
+    //        .OnComplete(() => ToggleActive(canvasList[1], canvasList[2]));        
+    //}
+
+    //public void ToggleActive(GameObject obj1, GameObject obj2)
+    //{
+    //    obj1.SetActive(false);
+    //    obj2.SetActive(true);
+    //}
 
     void Fade(CanvasGroup canvasGroup, float endValue, float duration, TweenCallback onEnd)
     {
@@ -55,8 +74,17 @@ public class MenuManager : MonoBehaviour
     private IEnumerator FadeCanvas()
     {
         yield return new WaitForSeconds(1f);
-        FadeIn(canvasList[0].GetComponent<CanvasGroup>(), 1f);
+        FadeIn(canvasList[0].GetComponentInChildren<CanvasGroup>(), 1f);
         yield return new WaitForSeconds(3f);
-        FadeOut(canvasList[0].GetComponent<CanvasGroup>(), 1f);
+        FadeOut(canvasList[0].GetComponentInChildren<CanvasGroup>(), 1f);
+        yield return new WaitForSeconds(1.5f);
+        canvasList[0].SetActive(false);
+        canvasList[1].SetActive(true);
+        FadeOut(canvasList[1].GetComponentInChildren<CanvasGroup>(), 1f);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
